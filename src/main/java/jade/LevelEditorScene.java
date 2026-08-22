b2da0beb39009b5eac76de365050427200d8d7f9
Kristiyan Krastev <krystev.kristiyan@gmail.com>
2026-08-22 19:12:17 +0300

    feat: add a 2D camera with orthographic projection
    - Add Camera, holding a position, orthographic projection matrix,
      and a view matrix computed via lookAt
    - Add Scene.camera and initialize it in LevelEditorScene
    - Upload uProjection/uView matrices to the shader each frame via a
      new Shader.uploadMat4f() helper
    - Update default.glsl to transform vertices by uProjection * uView
    - Pan the camera left over time in LevelEditorScene as a smoke test

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,5 +1,6 @@
 package jade;
 
+import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
 
@@ -38,10 +39,10 @@ public class LevelEditorScene extends Scene {
     private int vertexID, fragmentID, shaderProgram;
     private float[] vertexArray = {
             // position               // color
-            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // bottom right 0
-            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // top left     1
-            0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right    2
-            -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, // bottom left  3
+            100.5f, 0.5f, 0.0f,    1.0f, 0.0f, 0.0f, 1.0f, // bottom right 0
+            0.5f, 100.5f, 0.0f,      0.0f, 1.0f, 0.0f, 1.0f, // top left     1
+            100.5f, 100.5f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f, // top right    2
+           0.5f, 0.5f, 0.0f,         1.0f, 1.0f, 0.0f, 1.0f, // bottom left  3
     };
     // IMPORTANT: Must be in counter-clockwise order
     private int[] elementArray = {
@@ -65,6 +66,7 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
+        this.camera = new Camera(new Vector2f());
         defaultShader = new Shader("assets/shaders/default.glsl");
 
         defaultShader.compile();
@@ -104,7 +106,10 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void update(float dt) {
+        camera.position.x -= dt * 50.0f;
         defaultShader.use();
+        defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
+        defaultShader.uploadMat4f("uView", camera.getViewMatrix());
         // Bind the VAO that we're using
         glBindVertexArray(vaoID);
 
